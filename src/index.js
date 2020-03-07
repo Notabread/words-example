1c0be216d1569885ed8eb860b728cef1365167c0
Notabread <yy1549024@gmail.com>
2020-03-07 17:47:44 +0300

    Added basic classes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Test from "./classes/Test";
+import Task from "./classes/Task";
+import Service from "./classes/Service";
+
+window.addEventListener("load", function () {
+
+    const app = new Test();
+
+
+});
