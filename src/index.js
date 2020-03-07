efc60d167ccd6fc0db24c4fdab6578d462c23a61
Notabread <yy1549024@gmail.com>
2020-03-07 20:31:01 +0300

    released task mixing, and others basic functions of class Test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import Test from "./classes/Test";
-import { Task, WordTask, PhraseTask, TranslateTask} from "./classes/Task";
-import Service from "./classes/Service";
 
 window.addEventListener("load", function () {
 
-    const app = new Test();
+    window.test = new Test();
 
 });
-
-const taskData = [
-    { type: "translate", question: "кот", answer: "cat" },
-    { type: "word", question: "кот"},
-];
-const tasks = taskData.map(Task.create);
-window.tasks = tasks;
