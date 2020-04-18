a03b335d5014059d5efff2daafd4cc7a556552c7
Notabread <yy1549024@gmail.com>
2020-04-18 13:59:09 +0300

    Added JSDocs for ui TaskModule

--- src/classes/ui/TaskModule.js
+++ src/classes/ui/TaskModule.js
@@ -1,26 +1,46 @@
 export default class TaskModule {
 
+    /**
+     * Задаём основные общие параметры и вызываем метод отрисовки
+     *
+     * @param data
+     */
     constructor(data) {
         this._test = data.test;
         this.infoBlock = document.querySelector("#question_info");
         this.questionBlock = document.querySelector("#letters");
         this.answerBlock = document.querySelector('#answer');
-        document.querySelector('#pop').style.display = 'none';
-        document.querySelector('#new_test').style.display = 'none';
         this._render(data);
     }
 
+    /**
+     * Относительно общий метод отрисовки
+     *
+     * @private
+     */
     _render() {
+        document.querySelector('#pop').style.display = 'none';
+        document.querySelector('#new_test').style.display = 'none';
         this._clearField();
         this._setCurrent();
     }
 
+    /**
+     * Функция очистки задач
+     *
+     * @private
+     */
     _clearField() {
         this.questionBlock.innerHTML = '';
         this.infoBlock.innerHTML = '';
         this.answerBlock.innerHTML = '';
     }
 
+    /**
+     * Фкункция индикатора текущей задачи (меняет номер текущей задачи)
+     *
+     * @private
+     */
     _setCurrent() {
         let { total, current } = this._test.state;
         this.totalBlock = document.querySelector("#total_questions");
@@ -29,6 +49,11 @@ export default class TaskModule {
         this.currentBlock.innerHTML = current + 1;
     }
 
+    /**
+     * Функция создания экземпляров по зарегистрированным ключам
+     *
+     * @param data
+     */
     static create(data) {
         const constructor = TaskModule._modules[data.type];
         if (constructor) {
@@ -37,6 +62,12 @@ export default class TaskModule {
         throw new Error(`UI: There is no constructor for ${data.type}!`);
     }
 
+    /**
+     * Функция регистрации новых модулей
+     *
+     * @param key
+     * @param constructor
+     */
     static register(key, constructor) {
         if (TaskModule._modules[key]) {
             throw new Error(`UI: Module ${key} already registered!`);
